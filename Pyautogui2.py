420b235f9bc7ebcafc6305398ccd420df23901cc
Ana Elisa Ferreira <anaelisaferreira444@gmail.com>
2025-08-23 11:52:08 -0300

    Create Pyautogui2.py

---
+++ Pyautogui2.py
@@ -0,0 +1,21 @@
+import pyautogui
+
+pyautogui.press("win")
+pyautogui.sleep(0.5)
+pyautogui.write("Google Chrome",interval=0.1)
+pyautogui.press("enter")
+pyautogui.sleep(2)
+
+pyautogui.click(871,589)
+pyautogui.write ("https://prefeitura.santanadeparnaiba.sp.gov.br/Plataforma/smti/fabrica-de-programadores",interval=0.1)
+pyautogui.press("enter")
+pyautogui.sleep(1)
+
+pyautogui.hotkey("win", "shift", "s")
+pyautogui.moveTo(10,10)
+pyautogui.sleep(0.5)
+pyautogui.mouseDown()
+pyautogui.sleep(0.5)
+pyautogui.moveTo(x=1900,y=1050, duration =1)
+pyautogui.sleep(2)
+pyautogui.mouseUp()
