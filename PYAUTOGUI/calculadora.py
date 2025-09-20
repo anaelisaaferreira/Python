fb75ad6209cb727248b7b077ac31f629d5c0ad15
Aluno Programador 5 <prog5@pmsp.edu.local>
2025-09-20 10:04:41 -0300

    Organização dos programas

---
+++ PYAUTOGUI/calculadora.py
@@ -0,0 +1,13 @@
+import pyautogui
+
+pyautogui.hotkey("win","r")
+pyautogui.sleep(1)
+
+pyautogui.write("calc",interval=0.1)
+pyautogui.press("enter")
+pyautogui.sleep(1)
+
+pyautogui.write("20")
+pyautogui.press("+")
+pyautogui.write("20")
+pyautogui.press("enter")
--- cadastro.py
+++
@@ -1,9 +0,0 @@
-# Solicitando os dados para o cadrasto
-nome_completo = input ("Digite seu nome: ")
-email = input ("Digite seu gmail: ")
-
-# Criando o arquivo pessoa.txt para gravação dos dados
-arquivo = open ("pessoa.txt", "a")
-arquivo.write(f"{nome_completo} | {email}")
-arquivo.close()
-
